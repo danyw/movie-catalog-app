211f1fc95e04ec9f5fcd3855a21771fd0e65ec06
danyw <danielwiejak@gmai.com>
2023-03-18 13:58:24 +0000

    fix issues after merging auth0 (version-1)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,31 @@
-import { Container } from 'react-bootstrap'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import LoginButton from '../Auth/Login'
-import LogoutButton from '../Auth/Logout'
-import Profile from '../Auth/Profile'
-import { useAuth0 } from '@auth0/auth0-react'
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import LoginButton from "../Auth/Login";
+import LogoutButton from "../Auth/Logout";
+import Profile from "../Auth/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
+import logo from "../Assets/Logo.png";
 
 const Header = () => {
-  const { isAuthenticated } = useAuth0()
+  const { isAuthenticated } = useAuth0();
   return (
     <Container>
-      <Navbar bg='dark' variant='dark'>
+      <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href='/'>Navbar</Navbar.Brand>
-          <Nav className='me-auto'>
-            <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/fav'>Features</Nav.Link>
-            <Nav.Link href='/about'>Pricing</Nav.Link>
+          <Navbar.Brand href="/">
+            <img src={logo} width="30" height="30" alt="Our Logo" />
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/fav">Favourites</Nav.Link>
+            <Nav.Link href="/about">ABout Us</Nav.Link>
           </Nav>
 
-          <Nav className='justify-content-end' activeKey='/home'>
+          <Nav className="justify-content-end" activeKey="/home">
             {!isAuthenticated && (
               <Nav.Item>
-                <Nav.Link href='/home'>
+                <Nav.Link href="/home">
                   <LoginButton />
                 </Nav.Link>
               </Nav.Item>
@@ -31,12 +34,12 @@ const Header = () => {
             {isAuthenticated && (
               <>
                 <Nav.Item>
-                  <Nav.Link eventKey='link-2'>
+                  <Nav.Link eventKey="link-2">
                     <Profile />
                   </Nav.Link>
                 </Nav.Item>
                 <Nav.Item>
-                  <Nav.Link eventKey='link-1'>
+                  <Nav.Link eventKey="link-1">
                     <LogoutButton />
                   </Nav.Link>
                 </Nav.Item>
@@ -46,7 +49,7 @@ const Header = () => {
         </Container>
       </Navbar>
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
