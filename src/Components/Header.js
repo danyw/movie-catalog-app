7b294e49cc8e53867c34b9c827e271d4d543765e
danyw <danielwiejak@gmai.com>
2023-03-18 12:31:21 +0000

    add navbar, add router

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,23 @@
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 const Header = () => {
-    return ( 
-        <>
-        {/* auth0 button here */}
-        </>
-     );
-}
- 
+  return (
+    <Container>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/fav">Features</Nav.Link>
+            <Nav.Link href="/about">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </Container>
+  );
+};
+
 export default Header;
