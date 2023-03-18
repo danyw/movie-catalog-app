c42582788079e20015ed44854a08937a9605de8f
danyw <danielwiejak@gmail.com>
2023-03-18 13:16:02 +0000

    add search

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,26 @@
+import React from "react";
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from "../Assets/Logo.png";
 
 const Header = () => {
   return (
     <Container>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img
+            src={logo}
+            width="30"
+            height="30"
+            alt="Our Logo"
+            />
+            </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/fav">Features</Nav.Link>
-            <Nav.Link href="/about">Pricing</Nav.Link>
+            <Nav.Link href="/fav">Favourites</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,12 @@
+import Search from "../Components/Search";
+
 const Home = () => {
-    return ( 
-        <>  
-        <h2>Home</h2>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <h2>Home</h2>
+      <Search />
+    </>
+  );
+};
+
 export default Home;
