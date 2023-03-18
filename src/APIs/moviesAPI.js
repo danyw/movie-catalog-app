788c517b3a9eba155f1f24ccd0c1b40302a85ef7
danyw <danielwiejak@gmail.com>
2023-03-18 18:31:25 +0000

    add get data from themoviedb

--- src/APIs/moviesAPI.js
+++ src/APIs/moviesAPI.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
 const SERVER = process.env.REACT_APP_SERVER;
+const API_key = process.env.REACT_APP_API_KEY;
 
-export const searchMoviesAPI = async (search) => {
-
+export const searchMoviesAPI = async (search) => { 
 try {
-    const response = await axios.get(`${SERVER}/movielist`, { search });
-return console.log(response.data);
+    const response = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_key}&query=${search}`);
+ console.log(response.data);
 } catch (error) {
     console.error(error);
 }
 };
+
 
 export const addToFav = () => {
 }
