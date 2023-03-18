1ce4b5f5cc3200f07912998a8e880c926caa39c6
ermias kidane <ermiaskidanegebre@gmail.com>
2023-03-18 13:35:05 +0000

    added auth0

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "./Styles/search.css";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Auth0Provider } from '@auth0/auth0-react'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain='dev-4fae3hrgzah5n8mx.eu.auth0.com'
+    clientId='ZZq4a9A2klChbdpk0KpZvv5VIblPOFwW'
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
-);
+  </Auth0Provider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
