58168b50f6dcaabfda31050be094a810311adc6b
danyw <danielwiejak@gmail.com>
2023-03-18 11:58:58 +0000

    add router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Favourites from './Pages/Favourites';
+import AboutUs from './Pages/AboutUs';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Testing</h1>
+      <Router>
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fav" element={<Favourites />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/Favourites.js
@@ -0,0 +1,9 @@
+const Favourites = () => {
+    return ( 
+        <> 
+        <h2>Favourites</h2>
+         </>
+     );
+}
+ 
+export default Favourites;
