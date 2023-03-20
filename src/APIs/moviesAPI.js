2900ed9013d180d05affd18fdb7b9f4b129c6681
danyw <danielwiejak@gmail.com>
2023-03-20 02:54:21 +0000

    fix css styling, add rendering cards

--- src/APIs/moviesAPI.js
+++ src/APIs/moviesAPI.js
@@ -1,25 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 const SERVER = process.env.REACT_APP_SERVER;
 const API_key = process.env.REACT_APP_API_KEY;
 
-export const searchMoviesAPI = async (search) => { 
-try {
+export const searchMoviesAPI = async (search) => {
+  try {
     const response = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_key}&query=${search}`);
- console.log(response.data);
-} catch (error) {
+   //TODO: replace multi with movie title or actor name
+    return response.data;
+  } catch (error) {
     console.error(error);
-}
+    return null;
+  }
 };
 
+export const addToFav = () => {};
 
-export const addToFav = () => {
-}
+export const getFav = () => {};
 
-export const getFav = () => {
-}
-
-export const deleteFromFav = () => {
-}
-
-
+export const deleteFromFav = () => {};
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,19 @@
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
-import Img from '../Assets/adams.jpg'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function CardImage(props) {
+  const img = `https://image.tmdb.org/t/p/w500/${props.movies.poster_path}`;
   return (
-    <Card style={{ width: '18rem' }}>
-      <p>{props.movies}</p>
-      <Card.Img variant='top' src={Img} />
+    <Card style={{ width: "18rem" }}>
+      {/* <p>{props.movies}</p> */}
+      <Card.Img variant="top" src={img} />
       <Card.Body>
-        <Card.Title>Card Title {props.title}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant='primary'>Go somewhere</Button>
+        <Card.Title>{props.movies.title}</Card.Title>
+        <Card.Text>{props.movies.release_date}</Card.Text>
+        <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default CardImage
+export default CardImage;
