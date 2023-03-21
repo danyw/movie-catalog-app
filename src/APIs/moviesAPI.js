a15bf54d11001e149bb9ecc969514960eaf67a87
danyw <danielwiejak@gmail.com>
2023-03-21 01:08:46 +0000

    add modal, fetch cast/crew by id

--- src/APIs/moviesAPI.js
+++ src/APIs/moviesAPI.js
@@ -6,7 +6,16 @@ const API_key = process.env.REACT_APP_API_KEY;
 export const searchMoviesAPI = async (search) => {
   try {
     const response = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${API_key}&query=${search}`);
-   //TODO: replace multi with movie title or actor name
+    //TODO: replace multi with movie title or actor name
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+export const getCreditsByIdAPI = async (id) => {
+  try {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_key}&language=en-US`);
     return response.data;
   } catch (error) {
     console.error(error);
